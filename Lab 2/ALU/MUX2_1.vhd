b054a829a4d15a3aafe10ecf0f91e66af461873d
anijjar <anijjar@uvic.ca>
2021-02-18 12:21:27 -0800

    built Right barrel shifter

--- Lab 2/ALU/MUX2_1.vhd
+++ Lab 2/ALU/MUX2_1.vhd
@@ -13,11 +13,6 @@ ENTITY MUX2_1 IS
 END MUX2_1;
 
 ARCHITECTURE behavioural OF MUX2_1 IS
-
 BEGIN
-   PROCESS (SEL, A, B)
-   BEGIN
-      C <= A WHEN (SEL = '1') ELSE B;
-   END PROCESS;
-
+   C <= A WHEN (SEL = '1') ELSE B;
 END behavioural;
--- Lab 2/ALU/RBS_16.vhd
+++ Lab 2/ALU/RBS_16.vhd
@@ -20,66 +20,156 @@ ARCHITECTURE behavioural OF RBS_16 IS
       );
    END COMPONENT;
 
-   SIGNAL c0 : STD_LOGIC_VECTOR (31 DOWNTO 0);
-   SIGNAL c1 : STD_LOGIC_VECTOR (31 DOWNTO 0);
-   SIGNAL c2 : STD_LOGIC_VECTOR (31 DOWNTO 0);
-   SIGNAL c3 : STD_LOGIC_VECTOR (31 DOWNTO 0);
+   SIGNAL d : STD_LOGIC_VECTOR (31 DOWNTO 0);
+   TYPE array_puts IS ARRAY (INTEGER RANGE 0 TO 4) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
 BEGIN
-   -- setup c3 to input the d_in signal of the array
-   -- needs to be d_in(15), 0,  d_in(14), 0,  d_in(13), 0, d_in(12), 0, d_in(11), 0, d_in(10), 0, d_in(9), 0, d_in(8) 0, d_in(7), d_in(15), d_in(6), d_in(14), d_in(5), d_in(13), d_in(4), d_in(12), d_in(3), d_in(11), d_in(2), d_in(10), d_in(1), d_in(9), d_in(0), d_in(8)
-   for elem in 31 downto 0 loop
-      c3(elem) <= d_in(elem) when (elem mod 2 = 1) else 
-            '0' when (elem >= 16) else
-            d_in((elem*2)+3) when (elem mod 2 = 0);
-      c2 <= c3(elem) when (elem mod 2 = 1) else 
-            '0' when (elem >= 24) else
-            d_in((elem)+3) when (elem mod 2 = 0);
-            ;
-      c1 <= c2(elem) when (elem mod 2 = 1) else 
-            '0' when (elem >= 28);
-      c0 <= c1(elem) when (elem mod 2 = 1) else 
-            '0' when (elem >= 30); 
-   end loop
 
-   array: for i in 3 downto 0 generate
-      -- for the first row we have RBS input
-      row3: if (i = 3) generate
-         u : MUX_ARRAY PORT MAP(
-            d_in => c3,
-            b => shift(3),
-            d_out => c2,
-         )
-      end generate row3;
+   array_puts(4) <= d_in;
 
-      row2: if (i = 2) generate
-         u : MUX_ARRAY PORT MAP(
-            d_in => c2,
-            b => shift(2),
-            d_out => c1,
-         )
-      end generate row2;
-
-      row1: if (i = 1) generate
-         u : MUX_ARRAY PORT MAP(
-            d_in => c1,
-            b => shift(1),
-            d_out => c0,
-         )
-      end generate row1;
-
-      row0: if (i = 0) generate
-         u : MUX_ARRAY PORT MAP(
-            d_in => c0,
-            b => shift(0),
-            d_out => d_out,
-         )
-      end generate row0;
-
-   end generate array;
-
-   SHR : PROCESS (d_in, shift)
-   BEGIN
-
-   END PROCESS;
+   u3 : MUX_ARRAY PORT MAP(
+      d_in(0) => array_puts(4)(8),
+      d_in(1) => array_puts(4)(0),
+      d_in(2) => array_puts(4)(9),
+      d_in(3) => array_puts(4)(1),
+      d_in(4) => array_puts(4)(10),
+      d_in(5) => array_puts(4)(2),
+      d_in(6) => array_puts(4)(11),
+      d_in(7) => array_puts(4)(3),
+      d_in(8) => array_puts(4)(12),
+      d_in(9) => array_puts(4)(4),
+      d_in(10) => array_puts(4)(13),
+      d_in(11) => array_puts(4)(5),
+      d_in(12) => array_puts(4)(14),
+      d_in(13) => array_puts(4)(6),
+      d_in(14) => array_puts(4)(15),
+      d_in(15) => array_puts(4)(7),
+      d_in(16) => '0',
+      d_in(17) => array_puts(4)(8),
+      d_in(18) => '0',
+      d_in(19) => array_puts(4)(9),
+      d_in(20) => '0',
+      d_in(21) => array_puts(4)(10),
+      d_in(22) => '0',
+      d_in(23) => array_puts(4)(11),
+      d_in(24) => '0',
+      d_in(25) => array_puts(4)(12),
+      d_in(26) => '0',
+      d_in(27) => array_puts(4)(13), 
+      d_in(28) => '0',
+      d_in(29) => array_puts(4)(14),
+      d_in(30) => '0',
+      d_in(31) => array_puts(4)(15),
+      shift => shift(3),
+      d_out => array_puts(3),
+   );
+   u2 : MUX_ARRAY PORT MAP(
+      d_in(0) => array_puts(3)(4),
+      d_in(1) => array_puts(3)(0),
+      d_in(2) => array_puts(3)(5),
+      d_in(3) => array_puts(3)(1),
+      d_in(4) => array_puts(3)(6),
+      d_in(5) => array_puts(3)(2),
+      d_in(6) => array_puts(3)(7),
+      d_in(7) => array_puts(3)(3),
+      d_in(8) => array_puts(3)(8),
+      d_in(9) => array_puts(3)(4),
+      d_in(10) => array_puts(3)(9),
+      d_in(11) => array_puts(3)(5),
+      d_in(12) => array_puts(3)(10),
+      d_in(13) => array_puts(3)(6),
+      d_in(14) => array_puts(3)(11),
+      d_in(15) => array_puts(3)(7),
+      d_in(16) => array_puts(3)(12),
+      d_in(17) => array_puts(3)(8),
+      d_in(18) => array_puts(3)(13),
+      d_in(19) => array_puts(3)(9),
+      d_in(20) => array_puts(3)(14),
+      d_in(21) => array_puts(3)(10),
+      d_in(22) => array_puts(3)(15),
+      d_in(23) => array_puts(3)(11),
+      d_in(24) => '0',
+      d_in(25) => array_puts(3)(12),
+      d_in(26) => '0',
+      d_in(27) => array_puts(3)(13), 
+      d_in(28) => '0',
+      d_in(29) => array_puts(3)(14),
+      d_in(30) => '0',
+      d_in(31) => array_puts(3)(15),
+      shift => shift(2),
+      d_out => array_puts(2),
+   );
+   u1 : MUX_ARRAY PORT MAP(
+      d_in(0) => array_puts(2)(2),
+      d_in(1) => array_puts(2)(0),
+      d_in(2) => array_puts(2)(3),
+      d_in(3) => array_puts(2)(1),
+      d_in(4) => array_puts(2)(4),
+      d_in(5) => array_puts(2)(2),
+      d_in(6) => array_puts(2)(5),
+      d_in(7) => array_puts(2)(3),
+      d_in(8) => array_puts(2)(6),
+      d_in(9) => array_puts(2)(4),
+      d_in(10) => array_puts(2)(7),
+      d_in(11) => array_puts(2)(5),
+      d_in(12) => array_puts(2)(8),
+      d_in(13) => array_puts(2)(6),
+      d_in(14) => array_puts(2)(9),
+      d_in(15) => array_puts(2)(7),
+      d_in(16) => array_puts(2)(10),
+      d_in(17) => array_puts(2)(8),
+      d_in(18) => array_puts(2)(11),
+      d_in(19) => array_puts(2)(9),
+      d_in(20) => array_puts(2)(12),
+      d_in(21) => array_puts(2)(10),
+      d_in(22) => array_puts(2)(13),
+      d_in(23) => array_puts(2)(11),
+      d_in(24) => array_puts(2)(14), 
+      d_in(25) => array_puts(2)(12),
+      d_in(26) => array_puts(2)(15), 
+      d_in(27) => array_puts(2)(13), 
+      d_in(28) => '0',
+      d_in(29) => array_puts(2)(14),
+      d_in(30) => '0',
+      d_in(31) => array_puts(2)(15),
+      shift => shift(1),
+      d_out => array_puts(1),
+   );
+   u0 : MUX_ARRAY PORT MAP(
+      d_in(0) => array_puts(1)(1),
+      d_in(1) => array_puts(1)(0),
+      d_in(2) => array_puts(1)(2),
+      d_in(3) => array_puts(1)(1),
+      d_in(4) => array_puts(1)(3),
+      d_in(5) => array_puts(1)(2),
+      d_in(6) => array_puts(1)(4),
+      d_in(7) => array_puts(1)(3),
+      d_in(8) => array_puts(1)(5),
+      d_in(9) => array_puts(1)(4),
+      d_in(10) => array_puts(1)(6),
+      d_in(11) => array_puts(1)(5),
+      d_in(12) => array_puts(1)(7),
+      d_in(13) => array_puts(1)(6),
+      d_in(14) => array_puts(1)(8),
+      d_in(15) => array_puts(1)(7),
+      d_in(16) => array_puts(1)(9),
+      d_in(17) => array_puts(1)(8),
+      d_in(18) => array_puts(1)(10),
+      d_in(19) => array_puts(1)(9),
+      d_in(20) => array_puts(1)(11),
+      d_in(21) => array_puts(1)(10),
+      d_in(22) => array_puts(1)(12),
+      d_in(23) => array_puts(1)(11),
+      d_in(24) => array_puts(1)(13), 
+      d_in(25) => array_puts(1)(12),
+      d_in(26) => array_puts(1)(14), 
+      d_in(27) => array_puts(1)(13), 
+      d_in(28) => array_puts(1)(15),
+      d_in(29) => array_puts(1)(14),
+      d_in(30) => '0',
+      d_in(31) => array_puts(1)(15),
+      shift => shift(0),
+      d_out => array_puts(0),
+   );
+   d_out <= array_puts(0);
 
 END behavioural;
