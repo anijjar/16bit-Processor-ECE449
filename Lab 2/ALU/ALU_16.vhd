573d54de8463563bd0ba61b5b06ca5a43ce04804
anijjar <anijjar@uvic.ca>
2021-02-18 18:04:49 -0800

    RBS_16 runs correct;y. write LBS_16

--- Lab 2/ALU/ALU_16.vhd
+++ Lab 2/ALU/ALU_16.vhd
@@ -1,79 +1,72 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
-use ieee.numeric_std.ALL;
-use IEEE.std_logic_signed.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+USE ieee.numeric_std.ALL;
+USE IEEE.std_logic_signed.ALL;
 
 ENTITY ALU_16 IS
-    PORT(
-        -- master control inputs
-        rst : IN std_logic;
-        clk : IN std_logic;
-        -- control INput
-        alu_mode : IN std_logic_vector(2 DOWNTO 0);
-        -- data INputs
-        IN1 : IN std_logic_vector(15 DOWNTO 0);
-        IN2 : IN std_logic_vector(15 DOWNTO 0);
-        -- data output
-        result : out std_logic_vector(15 DOWNTO 0);
-        -- control output
-        -- ov_flag : out std_logic;
-        z_flag : out std_logic;
-        n_flag : out std_logic);
+   PORT (
+      -- master control inputs
+      rst : IN STD_LOGIC;
+      clk : IN STD_LOGIC;
+      -- control INput
+      alu_mode : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+      -- data INputs
+      IN1 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      IN2 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      -- data output
+      result : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+      -- control output
+      -- ov_flag : out std_logic;
+      z_flag : OUT STD_LOGIC;
+      n_flag : OUT STD_LOGIC);
 END ALU_16;
 
 ARCHITECTURE behavioural OF ALU_16 IS
-    SIGNAL ra : std_logic_vector(15 DOWNTO 0);
-    SIGNAL rb : std_logic_vector(15 DOWNTO 0);
-    SIGNAL rc : std_logic_vector(15 DOWNTO 0);
-    SIGNAL cl : INTEGER;
-BEGIN 
-PROCESS(rst, alu_mode, in1, in2)
+
+   RBS_16 : ENTITY work.RBS_16 PORT MAP(RBS_16_D_IN, RBS_16_SHIFT, RBS_16_D_OUT);
+   SIGNAL ra : STD_LOGIC_VECTOR(15 DOWNTO 0);
+   SIGNAL rb : STD_LOGIC_VECTOR(15 DOWNTO 0);
+   SIGNAL rc : STD_LOGIC_VECTOR(15 DOWNTO 0);
+   SIGNAL cl : INTEGER;
+   
 BEGIN
-    IF (rst = '1') THEN
-        -- not sure if output needs to be set to zero
-        -- RF8_16 does not do this
-        result <= (others => '0');
-        z_flag <= '0';
-        n_flag <= '0';
-        -- set all internal values to default; zero
-    ELSE 
-        CASE alu_mode(2 DOWNTO 0) IS
+   PROCESS (rst, alu_mode, in1, in2)
+   BEGIN
+      IF (rst = '1') THEN
+         -- not sure if output needs to be set to zero
+         -- RF8_16 does not do this
+         result <= (OTHERS => '0');
+         z_flag <= '0';
+         n_flag <= '0';
+         -- set all internal values to default; zero
+      ELSE
+         CASE alu_mode(2 DOWNTO 0) IS
             WHEN "000" => -- NOP
-                result <= (others => '0');
-                z_flag <= '0';
-                n_flag <= '0';
-                -- set internal values to default?
+               result <= (OTHERS => '0');
+               z_flag <= '0';
+               n_flag <= '0';
+               -- set internal values to default?
             WHEN "001" => -- ADD
-                
+
             WHEN "010" => -- SUB
 
             WHEN "011" => -- MUL
 
             WHEN "100" => -- NAND
-                result <= in1 NAND in2;
+               result <= in1 NAND in2;
             WHEN "101" => -- SHL
-                ra <= in1; -- ra
-                cl <= TO_INTEGER(UNSIGNED(in2)); -- this is some constant 
---                result <= ra SLL cl WHEN cl > 0;
+               ra <= in1; -- ra
+               cl <= TO_INTEGER(UNSIGNED(in2)); -- this is some constant 
+               --                result <= ra SLL cl WHEN cl > 0;
             WHEN "110" => -- SHR
-                ra <= in1; -- ra
-                cl <= TO_INTEGER(UNSIGNED(in2)); -- this is some constant 
---                result <= ra SRL cl WHEN cl > 0;
+               ra <= in1; -- ra
+               cl <= TO_INTEGER(UNSIGNED(in2)); -- this is some constant 
+               --                
             WHEN "111" => -- TEST
-                ra <= in1;
-                IF (TO_INTEGER(SIGNED(ra)) = 0) THEN
-                    z_flag <= '1';
-                ELSE
-                    z_flag <= '0';
-                END IF;
-                IF (TO_INTEGER(SIGNED(ra)) < 0) THEN
-                    n_flag <= '1';
-                ELSE
-                    n_flag <= '0';
-                END IF;
+
             WHEN OTHERS => NULL;
-        END CASE;
-    END IF;
-END PROCESS;
-END behavioural ; -- behavioural
+         END CASE;
+      END IF;
+   END PROCESS;
+END behavioural; -- behavioural
--- Lab 2/ALU/shift/MUX_ARRAY.vhd
+++ Lab 2/ALU/shift/MUX_ARRAY.vhd
@@ -5,23 +5,25 @@ USE ieee.std_logic_unsigned.ALL;
 
 ENTITY MUX_ARRAY IS
    PORT (
-      d_in : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-      shift : IN STD_LOGIC;
-      d_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+      data_in : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+      s : IN STD_LOGIC;
+      data_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
    );
 END MUX_ARRAY;
 
 ARCHITECTURE behavioural OF MUX_ARRAY IS
    COMPONENT MUX2_1 IS
       PORT (
-         A, B, SEL : IN STD_LOGIC;
-         C : OUT STD_LOGIC);
+      SEL : IN STD_LOGIC;
+      A : IN STD_LOGIC;
+      B : IN STD_LOGIC;
+      C : OUT STD_LOGIC);
    END COMPONENT;
 
 BEGIN
 -- 7 for 8 bit shift, 15 for 16 bit shift
    gen: for i in 15 downto 0 generate
-      ut : MUX2_1 PORT MAP(B => d_in((i*2)+1), A => d_in(i*2), SEL => shift, C => d_out(i);
+      ut : MUX2_1 PORT MAP(s, data_in(i*2), data_in((i*2)+1), , data_out(i));
    end generate;
    
 END behavioural;
