10a9358c9fd8fbfb2c2e29cbccb3846b9df998cd
anijjar <anijjar@uvic.ca>
2021-04-08 14:55:55 -0700

    added assembler

---
+++ assembler/formatA/Test_FormatA_16Bit.asm
@@ -0,0 +1,27 @@
+	ORG	0x00
+	
+	IN R1
+	IN R2	
+	NOP
+	NOP
+	NOP
+	NOP
+	ADD R3, R2, R1
+	NOP
+	NOP
+	NOP
+	NOP
+	SHL R3, 2
+	NOP
+	NOP
+	NOP
+	NOP
+	MUL R2, R1, R3
+	NOP
+	NOP
+	NOP
+	NOP
+	OUT R2	
+	NOP
+
+	END
